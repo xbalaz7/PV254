eed4fd4de40eac5d7d4a48bd1b7608c8aeccd9f5
Marek Valko <valko.marek@gmail.com>
2018-11-14 18:39:57 +0100

    SampleData - demonstaration of basic App funcitonality
    It can now store gameids, which i want ot proces, and then downlaod and sotre games, reviews and suer, and relations between them

--- sampledata/src/main/java/cz/muni/fi/pv254/SampleDataLoadingFacadeImpl.java
+++ sampledata/src/main/java/cz/muni/fi/pv254/SampleDataLoadingFacadeImpl.java
@@ -1,8 +1,15 @@
 package cz.muni.fi.pv254;
 
+import cz.muni.fi.pv254.dao.UserDao;
+import cz.muni.fi.pv254.dto.GameDTO;
+import cz.muni.fi.pv254.dto.RecommendationDTO;
+import cz.muni.fi.pv254.dto.UserDTO;
 import cz.muni.fi.pv254.entity.Game;
 import cz.muni.fi.pv254.entity.Recommendation;
 import cz.muni.fi.pv254.entity.User;
+import cz.muni.fi.pv254.facade.GameFacade;
+import cz.muni.fi.pv254.facade.RecommendationFacade;
+import cz.muni.fi.pv254.facade.UserFacade;
 import cz.muni.fi.pv254.parsing.App;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -27,32 +34,78 @@ public class SampleDataLoadingFacadeImpl implements SampleDataLoadingFacade {
     @Autowired
     private RecommendationService recommendationService;
 
+    @Autowired
+    private UserFacade userFacade;
+
+    @Autowired
+    private GameFacade gameFacade;
+
+    @Autowired
+    private RecommendationFacade recommendationFacade;
+
+    @Autowired
+    private App app;
+
     @Override
     public void loadData() throws IOException {
-        User admin = user(123L,"password", "Admin", "admin@google.com", "766766766", "Praha", Boolean.TRUE);
-        User user = user(124L,"password", "User", "user@google.com", "755755755", "Olomouc", Boolean.FALSE);
-//        Game game = game(123, "game1");
-        App app = new App();
-        app.setDebug(0);
+//        User admin = user(123L,"password", "Admin", "admin@google.com", "766766766", "Praha", Boolean.TRUE);
+//        User user = user(124L, "password", "User", "user@google.com", "755755755", "Olomouc", Boolean.FALSE);
+//        System.out.println(userFacade.findById(user.getId()).toString());
+//        System.out.println(userFacade.findById(3L));
+//        System.out.println(userFacade.findBySteamId(124L).toString());
+//        System.out.println(userFacade.findBySteamId(125L));
+        //  TODO test game
+        app.setDebug(1);
         app.setOffsetDiff(100);
-//        int[] games = {892760, 911520, 964030,717690,949970,893330,396900,396900,582010,292030};
-        long[] games = {892760L};
-        for (long id : games) {
-            Game game = game(id, app.downloadGameName(id));
-            Set<List<Object>> ret = app.inteligentParse(id);
-            for (List<Object> rec : ret) {
-
-                Long userIdInt = (Long) rec.get(1);
-//                String userIdString = Long.toString((Long)rec.get(1));
-                String userIdString = app.downloadUserName(userIdInt);
-                String userIdEmail = userIdString + "@" + Long.toString(userIdInt) + ".com";
-                System.out.println(userIdString);
-                User author = user(userIdInt,userIdString, userIdString, userIdEmail, userIdString, userIdString, Boolean.FALSE);
-                Long recId = (Long) rec.get(0);
-                Boolean recVotedUp = (Boolean) rec.get(2);
-                Recommendation recommendation = recommendation(recId, recVotedUp, game, author);
+        app.addGameId(892760L);
+        app.addGameId(911520L);
+        app.addGameId(964030L);
+//        app.inteligentParse(892760L);
+        List<Integer> sizes = app.inteligentParseAllGanes();
+        System.out.println(sizes);
+        for (GameDTO game : gameFacade.findAll()) {
+            if (game != null) {
+                System.out.println(game.toString());
             }
+        }
+        for (UserDTO user : userFacade.findAll()) {
+            if (user != null) {
+                System.out.println(user.toString());
+            }
+        }
+        for (RecommendationDTO rec : recommendationFacade.findAll()) {
+            if (rec != null) {
+                System.out.println(rec.toString());
+            }
+        }
 
+
+        /**
+         * Old way of downloading/testing
+         */
+        if (false) {
+            app = new App();
+            app.setDebug(0);
+            app.setOffsetDiff(100);
+//        int[] games = {892760, 911520, 964030,717690,949970,893330,396900,396900,582010,292030};
+            long[] games = {892760L};
+            for (long id : games) {
+                Game game = game(id, app.downloadGameName(id));
+                Set<List<Object>> ret = app.inteligentParseOld(id);
+                for (List<Object> rec : ret) {
+
+                    Long userIdInt = (Long) rec.get(1);
+//                String userIdString = Long.toString((Long)rec.get(1));
+                    String userIdString = app.downloadUserName(userIdInt);
+                    String userIdEmail = userIdString + "@" + Long.toString(userIdInt) + ".com";
+                    System.out.println(userIdString);
+                    User author = user(userIdInt,userIdString, userIdString, userIdEmail, userIdString, userIdString, Boolean.FALSE);
+                    Long recId = (Long) rec.get(0);
+                    Boolean recVotedUp = (Boolean) rec.get(2);
+                    Recommendation recommendation = recommendation(recId, recVotedUp, game, author);
+                }
+
+            }
         }
     }
 
